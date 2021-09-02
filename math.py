2878337be843ecb01b2a9adeb779bfc9fab1a094
Shantanu2403 <89988536+Shantanu2403@users.noreply.github.com>
2021-09-02 21:31:12 +0530

    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,27 @@
+# Add implementation
+def add(x,y):
+	return x+y
+
+# Subtract implementation
+def subtract(x,y):
+    return x-y
+	
+# Multiply implementation
+
+def multiply (x,y):
+    return x*y
+
+# Divide implementation
+def divide(x,y):
+    if y==0:
+	return DIVIDE_BY_0_ERROR
+	else:
+	return x/y
+=======
+    return x/y	
+
+#Square implementation
+def square(x):
+pass
+
+	
